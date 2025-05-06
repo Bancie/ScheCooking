613a84723c1f0ff085f092ffdd4cfb92c7eaa4bf
Bancie <144613141+Bancie@users.noreply.github.com>
2025-05-06 15:31:06 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("quandang/vietnamese-foods")
+path = kagglehub.dataset_download("bosssssss/employee-schedule")
 
 print("Path to dataset files:", path)
